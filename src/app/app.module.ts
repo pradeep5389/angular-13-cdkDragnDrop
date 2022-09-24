b53a76fbfcc93a6fa1dddec9935566ea2a0c63bf
pradeep5389 <pradeep5389@users.noreply.github.com>
2022-09-24 13:04:50 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ListItemComponent } from './shared/components/list-item/list-item.component';
 import { CdkDragDropNestedListsExample } from './app.component';
+import { MaterialModule } from './material.module';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [ListItemComponent, CdkDragDropNestedListsExample],
-  imports: [BrowserModule, FormsModule, CommonModule, DragDropModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    DragDropModule,
+    MaterialModule,
+  ],
   entryComponents: [ListItemComponent, CdkDragDropNestedListsExample],
   providers: [],
   bootstrap: [CdkDragDropNestedListsExample],
